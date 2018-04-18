67191a4fd321a8a1872a0942b598797ddd26ea4e
caoyanqin0 <37920467+caoyanqin0@users.noreply.github.com>
2018-04-18 21:22:26 +0800

    Update window_wrapper.v

--- Hardware/v/window_wrapper.v
+++ Hardware/v/window_wrapper.v
@@ -3,20 +3,20 @@ module window_wrapper(
   input clock,
   input reset,
   // buffer inputs
-  input [`CAMERA_PIXEL_BITWIDTH:0] pixel_in,
+  input [`CAMERA_PIXEL_BITWIDTH:0] pixel_in,//8
   input shift_left,
   input shift_up,
   // window inputs
-  input [`X_COORD_BITWIDTH:0] kernel_x, // the bottom right corner of the kernel position
-  input [`Y_COORD_BITWIDTH:0] kernel_y, // the bottom right corner of the kernel position
+  input [`X_COORD_BITWIDTH:0] kernel_x, // the bottom right corner of the kernel position   4
+  input [`Y_COORD_BITWIDTH:0] kernel_y, // the bottom right corner of the kernel position   4
 
   // the kernel sized view of the buffer to be fed into the multipliers
-  output [`WINDOW_VECTOR_BITWIDTH:0] window_out 
+  output [`WINDOW_VECTOR_BITWIDTH:0] window_out //440
 );
 
 // wire declarations
-wire [`BUFFER_OUT_VECTOR_BITWIDTH:0] buffer_vector;
-wire [`CAMERA_PIXEL_BITWIDTH:0] window_wire [`KERNEL_SIZE-1:0][`KERNEL_SIZE-1:0];
+  wire [`BUFFER_OUT_VECTOR_BITWIDTH:0] buffer_vector;//7055
+  wire [`CAMERA_PIXEL_BITWIDTH:0] window_wire [`KERNEL_SIZE-1:0][`KERNEL_SIZE-1:0];//8,7,7
 
 // reg declarations
 
@@ -35,8 +35,8 @@ shifting_window shifting_window_inst(
 genvar i;
 genvar j;
 generate
-for (j=0; j<`KERNEL_SIZE; j=j+1) begin : selector_height_loop
-  for (i=0; i<`KERNEL_SIZE; i=i+1) begin : selector_width_loop
+  for (j=0; j<`KERNEL_SIZE; j=j+1) begin : selector_height_loop  //7
+    for (i=0; i<`KERNEL_SIZE; i=i+1) begin : selector_width_loop //7
     window_selector selector_inst(
       .clock(clock),
       .reset(reset),
@@ -55,7 +55,7 @@ generate
 for (n=0; n<`KERNEL_SIZE; n=n+1) begin : kernel_h_loop
   for (m=0; m<`KERNEL_SIZE; m=m+1) begin : kernel_w_loop
     assign window_out[
-    (`CAMERA_PIXEL_WIDTH*m)+(`KERNEL_SIZE*`CAMERA_PIXEL_WIDTH*n)+`CAMERA_PIXEL_BITWIDTH:
+    (`CAMERA_PIXEL_WIDTH*m)+(`KERNEL_SIZE*`CAMERA_PIXEL_WIDTH*n)+`CAMERA_PIXEL_BITWIDTH://9,7,9,8
     (`CAMERA_PIXEL_WIDTH*m)+(`KERNEL_SIZE*`CAMERA_PIXEL_WIDTH*n)
     ] = window_wire[m][n];
   end // for m
