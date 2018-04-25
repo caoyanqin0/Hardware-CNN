a7061c5c5977130acdc2396034da84a1b71984f5
caoyanqin0 <37920467+caoyanqin0@users.noreply.github.com>
2018-04-25 20:35:01 +0800

    Update nh_shift_reg.v

--- Hardware/v/nh_shift_reg.v
+++ Hardware/v/nh_shift_reg.v
@@ -4,7 +4,7 @@ module nh_shift_reg(
   input reset,
   
   input shift_in_rdy,
-  input [`NH_BITWIDTH:0] shift_in,
+  input [`NH_BITWIDTH:0] shift_in, //23
   
   output dval,
   output [`NH_BITWIDTH:0] nh_max
@@ -12,12 +12,12 @@ module nh_shift_reg(
 
 // wire declarations
 //wire [`NH_BITWIDTH:0] shift_ins [`NH_DIM-1:0]; 
-wire [(`NH_SIZE*2)-2:0] reg_valid;
-wire [`NH_BITWIDTH:0] nh_row_max [`NH_DIM-1:0];
-wire [`NH_BITWIDTH+1:0] nh_sr_wire [`NH_SR_DEPTH-1:0];
+  wire [(`NH_SIZE*2)-2:0] reg_valid; //4
+  wire [`NH_BITWIDTH:0] nh_row_max [`NH_DIM-1:0]; //23,2
+  wire [`NH_BITWIDTH+1:0] nh_sr_wire [`NH_SR_DEPTH-1:0]; //23,20
 
 // reg declarations
-reg [`NH_BITWIDTH+1:0] nh_reg [`NH_DIM-1:0][`NH_DIM-1:0];
+  reg [`NH_BITWIDTH+1:0] nh_reg [`NH_DIM-1:0][`NH_DIM-1:0]; //23,2,2
 
 // assign statments
 assign dval = reg_valid[0];
@@ -26,11 +26,11 @@ assign dval = reg_valid[0];
 //always@(*) begin
 always@(posedge clock or negedge reset) begin
   if( reset == 1'b0) begin
-    nh_reg[1][1] <= {1'd0, `NH_WIDTH'd0};
+    nh_reg[1][1] <= {1'd0, `NH_WIDTH'd0}; //24
     nh_reg[1][0] <= {1'd0, `NH_WIDTH'd0};
   end else begin
     nh_reg[1][1] <= {shift_in_rdy, shift_in};
-    nh_reg[1][0] <= nh_sr_wire[`NH_SR_DEPTH-1];
+    nh_reg[1][0] <= nh_sr_wire[`NH_SR_DEPTH-1]; //20
   end
 end
   
@@ -41,14 +41,14 @@ assign nh_sr_wire[0] = nh_reg[0][1];
 genvar i;
 genvar j;
 generate 
-for (j=0; j<`NH_DIM; j=j+1) begin : nh_row_loop
+for (j=0; j<`NH_DIM; j=j+1) begin : nh_row_loop  //2
   for (i=0; i<`NH_DIM-1; i=i+1) begin : le_sr_inst 
     // assign dval signal to wire 
 //    assign reg_valid[(j*`NH_DIM+i)+`NH_SIZE] = nh_reg[i][j][`NH_BITWIDTH+1];
     // connect nh le's into shift reg
     always@(posedge clock or negedge reset) begin
       if(reset == 1'b0) begin
-        nh_reg[i][j] <= `NH_WIDTH'd0;
+        nh_reg[i][j] <= `NH_WIDTH'd0; //24
       end else begin
         nh_reg[i][j] <= nh_reg[i+1][j];
       end // reset
@@ -61,7 +61,7 @@ generate
 for (j=0; j<`NH_DIM; j=j+1) begin : valid_row_loop
   for (i=0; i<`NH_DIM; i=i+1) begin : valid_height_loop 
     // assign dval signal to wire 
-    assign reg_valid[(j*`NH_DIM+i)+`NH_SIZE-1] = nh_reg[i][j][`NH_BITWIDTH+1];
+    assign reg_valid[(j*`NH_DIM+i)+`NH_SIZE-1] = nh_reg[i][j][`NH_BITWIDTH+1]; //2,4,23
   end // i
 end // j
 endgenerate
@@ -69,7 +69,7 @@ endgenerate
 // instantiate shift register to buffer the part of the line not in nh 
 genvar k;
 generate
-for( k=0; k<`NH_SR_DEPTH-1; k=k+1) begin : ram_sr_inst
+for( k=0; k<`NH_SR_DEPTH-1; k=k+1) begin : ram_sr_inst //20
   nh_sr_unit nh_sr_unit_inst(
     .clock(clock),
     .reset(reset),
@@ -82,7 +82,7 @@ endgenerate
 // AND the data valid signals
 genvar m;
 generate
-for (m=(`NH_SIZE*2)-2; m>=1; m=m-2) begin: or_loop
+for (m=(`NH_SIZE*2)-2; m>=1; m=m-2) begin: or_loop  //4
   assign reg_valid[(m/2)-1] = reg_valid[m] & reg_valid[m-1];
 end // for
 endgenerate
