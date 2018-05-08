f1c0223b8a40b432482318c097afcfd172573f54
caoyanqin0 <37920467+caoyanqin0@users.noreply.github.com>
2018-05-08 10:57:41 +0800

    Update np_matrix_mult_ctrl.v

--- Hardware/v/FFN/np_matrix_mult_ctrl.v
+++ Hardware/v/FFN/np_matrix_mult_ctrl.v
@@ -5,22 +5,22 @@ module np_matrix_mult_ctrl(
 
   input start,
   
-  output reg [`FM_ADDR_BITWIDTH:0] addr,
-  output reg [`RAM_SELECT_BITWIDTH:0] ram_select,
+  output reg [`FM_ADDR_BITWIDTH:0] addr,//6
+  output reg [`RAM_SELECT_BITWIDTH:0] ram_select,//2
   output reg mult_en,
   output reg product_rdy
 );
 
 always @(posedge clock or negedge reset) begin
   if (reset == 1'b0) begin
-    addr <= `FM_ADDR_WIDTH'd0;
-    ram_select <= `RAM_SELECT_WIDTH'd0;
+    addr <= `FM_ADDR_WIDTH'd0;//7
+    ram_select <= `RAM_SELECT_WIDTH'd0;//3
   end else begin
     if (start) begin
       addr <= `FM_ADDR_WIDTH'd0;
       ram_select <= `RAM_SELECT_WIDTH'd0;
-    end else if( addr == `ADDR_MAX - 1) begin
-      ram_select <= ram_select + `RAM_SELECT_WIDTH'd1;
+    end else if( addr == `ADDR_MAX - 1) begin//121
+      ram_select <= ram_select + `RAM_SELECT_WIDTH'd1;//3
       addr <= `FM_ADDR_WIDTH'd0;
     end else begin
       addr <= addr + `FM_ADDR_WIDTH'd1;
@@ -35,7 +35,7 @@ always@(posedge clock or negedge reset) begin
   if(reset == 1'b0) begin
     product_rdy <= 1'b0;
   end else begin
-    if(ram_select == `NUM_KERNELS - 1 & addr == `ADDR_MAX - 2 & mult_en) begin
+  if(ram_select == `NUM_KERNELS - 1 & addr == `ADDR_MAX - 2 & mult_en) begin//8,121
       product_rdy <= 1'b1;
     end else begin
       product_rdy <= 1'b0;
@@ -50,7 +50,7 @@ always@(posedge clock or negedge reset) begin
 	  mult_en <= 1'b0;
 	else if (start)
 	  mult_en <= 1'b1;
-	else if (ram_select == `NUM_KERNELS -1 & addr == `ADDR_MAX - 1)
+	else if (ram_select == `NUM_KERNELS -1 & addr == `ADDR_MAX - 1)//8,121
 	  mult_en <= 1'b0;
 	else
 	  mult_en <= mult_en;
